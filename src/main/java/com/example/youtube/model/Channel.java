16631558b3bd1d13bc9cd6ac729f3a148d630ab2
Sunrise <akselvolkov40@gmail.com>
2025-02-16 19:38:00 +0500

    adwghawha

--- src/main/java/com/example/youtube/model/Channel.java
+++ src/main/java/com/example/youtube/model/Channel.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class Channel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    private UUID id;    
 
     @ManyToOne
     @JoinColumn(name = "profile_id", nullable = false)
